9dfeda446275039eea1a30ecf67838d23df5fc9b
GabrielReyesOrtiz <greyes17@ucol.mx>
2020-12-07 17:56:41 -0600

    Implemented sumatoriaSerieDos

--- src/main.js
+++ src/main.js
@@ -10,8 +10,13 @@ export default class App {
     let suma = 1;
     let i = 2;
     while (i <= numero) {
-      suma = suma + 1 / i;
-      i++;
+      if (i % 2 == 0) {
+        suma = suma + 1 / i;
+        i++;
+      } else {
+        suma = suma - 1 / i;
+        i++;
+      }
     }
     return suma;
   }
